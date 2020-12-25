07a7e34490c21548808e874b98da785879ea044c
zhangyougaoxiong <1327588054@qq.com>
2020-12-25 16:38:40 +0800

    dockerFile修改

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,16 @@ ARG JAR_FILE=target/rcs-1.0.0.jar
 
 WORKDIR /opt/rcs
 ADD ${JAR_FILE} /opt/rcs/rcs.jar
-EXPOSE 8088
-CMD java -jar /opt/rcs/rcs.jar
+
+
+#java 启动参数
+ENV JAVA_OPTS="-Xmx256m -Xms256m"
+#web端口
+ENV port="8080"
+
+EXPOSE $port
+
+ENV nacos_ip="127.0.0.1"
+ENV nacos_port="8848"
+
+CMD java ${JAVA_OPTS} -jar /opt/rcs/rcs.jar --server.port=$port --spring.cloud.nacos.discovery.server-addr=$nacos_ip:$nacos_port
--- src/main/java/com/huawei/rcs/utils/XmlUtil.java
+++ src/main/java/com/huawei/rcs/utils/XmlUtil.java
@@ -1,96 +1,16 @@
 package com.huawei.rcs.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.ValueFilter;
-import com.sun.xml.internal.bind.marshaller.CharacterEscapeHandler;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.QNameMap;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
-import java.io.StringReader;
-import java.io.StringWriter;
 
 @Slf4j
 public class XmlUtil {
 
     private static final String xmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-
-    /**
-     * jaxb将对象转成xml字符
-     *
-     * @param obj java对象
-     * @return
-     */
-    public static String toXml(Object obj) {
-        String xmlString = "";
-        try {
-            JAXBContext context = JAXBContext.newInstance(obj.getClass());
-            Marshaller m = context.createMarshaller();
-            // 不进行转义字符的处理
-            m.setProperty(CharacterEscapeHandler.class.getName(), (CharacterEscapeHandler) (ch, start, length, isAttVal, writer) -> writer.write(ch, start, length));
-
-            //去掉standalone="yes"
-            m.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
-            StringWriter sw = new StringWriter();
-            m.marshal(obj, sw);
-            xmlString = sw.toString();
-
-        } catch (JAXBException e) {
-            log.error("JAXBException", e);
-            return null;
-        }
-
-        return xmlHeader + xmlString;
-    }
-
-    /**
-     * jaxb将对象转成xml字符
-     *
-     * @param obj               java对象
-     * @param ignoreEmptyString 是否忽略空字符串
-     * @return
-     */
-    public static String toXml(Object obj, boolean ignoreEmptyString) {
-        if (ignoreEmptyString) {
-            String objStr = JSON.toJSONString(obj, (ValueFilter) (object, name, value) -> {
-                if ("".equals(value)) {
-                    return null;
-                }
-                return value;
-            });
-            return toXml(JSON.parseObject(objStr, obj.getClass()));
-        } else {
-            return toXml(obj);
-        }
-    }
-
-    /**
-     * jaxb 将xml字符串转成java对象
-     *
-     * @param xmlStr
-     * @param clazz
-     * @param <T>
-     * @return
-     */
-    public static <T> T toBean(String xmlStr, Class clazz) {
-        JAXBContext context;
-        try {
-            context = JAXBContext.newInstance(clazz);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            T t = (T) unmarshaller.unmarshal(new StringReader(xmlStr.trim()));
-            return t;
-        } catch (JAXBException e) {
-            log.error("JAXBException", e);
-        }
-        return null;
-    }
-
 
     /**
      * xstream将xml字符串转成java对象
